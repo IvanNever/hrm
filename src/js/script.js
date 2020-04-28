caa1b6b02aaddaaf6b2ecdb75ee5b2449dd60cf8
Ivan Neverovsky <ivan@neverovsky.com.ua>
2020-04-28 17:41:32 +0300

    validate forms

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+    //Slider
     $('.carousel__inner').slick({
         speed: 1200,
         adaptiveHeight: true,
@@ -15,12 +17,12 @@ $(document).ready(function(){
         ],
     });
 
+    //Catalog tabs
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
         $(this)
           .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
           .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
       });
-
     function toggleSlide (item) {
         $(item).each(function(i) {
             $(this).on('click', function(e) {
@@ -30,25 +32,47 @@ $(document).ready(function(){
             })
         });
     };
-
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
 
     //Modal
-
     $('[data-modal=consultation]').on('click', function() {
         $('.overlay, #consultation').fadeIn('slow');
     });
-
     $('.modal__close').on('click', function() {
         $('.overlay, .modal').fadeOut('slow');
     });
-
     $('.button_mini').each( function(i) {
         $(this).on('click', function() {
             $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
             $('.overlay, #order').fadeIn('slow');
         });
     });
+
+    //Validation
+    function valideForm (form) {
+        $(form).validate({
+            rules: {
+                name: 'required',
+                phone: 'required',
+                email: {
+                    required: true,
+                    email: true
+                },
+            },
+            messages: {
+                name: 'Пожалуйста введите свое имя',
+                phone: 'Пожалуйста укажите номер телефона',
+                email: {
+                    required: 'Пожалуйста введите свою почту',
+                    email: 'Укажите правильный адрес почты'
+                },
+            },
+        });
+    };
+    valideForm('#consultation-form');
+    valideForm('#consultation form');
+    valideForm('#order form');
+
 
   });
