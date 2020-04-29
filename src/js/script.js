cd803f7666e052333fbd1a9acefd483fb05c2048
Ivan Neverovsky <ivan@neverovsky.com.ua>
2020-04-29 12:15:51 +0300

    Smooth scroll and page up

--- src/js/script.js
+++ src/js/script.js
@@ -93,5 +93,21 @@ $(document).ready(function(){
         return false;
     });
 
+    //Smooth scroll and page up
+    $(window).scroll( function() {
+        if($(this).scrollTop() > 1200) {
+            $('.page-up').fadeIn();
+        } else {
+            $('.page-up').fadeOut();
+        }
+    });
+    $(function(){
+        $("a[href^='#']").click(function(){
+                const _href = $(this).attr("href");
+                $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+                return false;
+    });
+
+});
 
   });
